218ea940288558256e9f9b1ae6f6e3edae2fd39f
unknown <Trung Tran>
2024-11-07 12:11:32 +0700

    Add RGB LED to circuit

--- src/RFID_RDM6300_SDCard/RFID_RDM6300_SDCard.ino
+++ src/RFID_RDM6300_SDCard/RFID_RDM6300_SDCard.ino
@@ -10,16 +10,29 @@
 #define RDM6300_TX_PIN 2  //Chân TX của RDM6300
 //#define READ_LED_PIN 13         //Led có sẵn trên Arduino
 #define WRITE_BUZZER_PIN 7      //Chân + của còi buzzer
+#define GREEN_LED_RGB 6    //Chân xanh lá của led rgb
+#define RED_LED_RGB 5     //Chân đỏ của led rgb
 #define SD_CARD_CHIP_SELECT 10  //Chân CS của module micro sd card spi
 
 Rdm6300 rdm6300;
 File dataFile;
 bool isCOMConnected;
 
+void changeLedColor(int red, int green)
+{
+  analogWrite(RED_LED_RGB, 255 - red);
+  analogWrite(GREEN_LED_RGB, 255 - green);
+}
+
 void setup() {
   Serial.begin(9600);
 
   pinMode(WRITE_BUZZER_PIN, OUTPUT);
+  pinMode(GREEN_LED_RGB, OUTPUT);
+  pinMode(RED_LED_RGB, OUTPUT);
+
+  //Để led rgb sáng màu vàng
+  //changeLedColor(185, 255);
 
   rdm6300.begin(RDM6300_TX_PIN);
 
@@ -27,9 +40,11 @@ void setup() {
 }
 
 void loop() {
+  changeLedColor(!isCOMConnected * 170, 255);
   //Kiếm tra xem thẻ nhớ có đang lắp vào module không
   //nếu không thì cho còi kêu
   while (!SD.begin(SD_CARD_CHIP_SELECT)) {
+    changeLedColor(255, 0);
     digitalWrite(WRITE_BUZZER_PIN, HIGH);
     delay(500);
     digitalWrite(WRITE_BUZZER_PIN, LOW);
@@ -56,9 +71,7 @@ void loop() {
   } else if (Serial.available() > 0) {  //nếu nhận được lệnh từ serial
     isCOMConnected = true;              //chuyển trạng thái kết nối cổng COM
     String commandReceived = Serial.readString();
-
-    //cần phải substring vì trong lệnh còn các ký tự kết thúc dòng
-    if (commandReceived.substring(0, 12) == "printTXTFile") {
+    if (commandReceived == "printTXTFile") {
       dataFile = SD.open("diemdanh.txt", FILE_READ);
       if (dataFile.size() == 0) {
         Serial.print("sdcard_empty");
@@ -68,14 +81,15 @@ void loop() {
         }
       }
 
-    } else if (commandReceived.substring(0, 20) == "prepareForDisconnect") {
+    } else if (commandReceived == "prepareForDisconnect") {
       SD.remove("diemdanh.txt");
       dataFile = SD.open("diemdanh.txt", FILE_WRITE);
       dataFile.close();
+    } else if (commandReceived == "startup") {
+
     }
   }
   //Bỏ chân 13 vì module thẻ nhớ đã dùng chân 13 rồi
   //digitalWrite(READ_LED_PIN, rdm6300.get_tag_id());
-
   delay(10);
 }
